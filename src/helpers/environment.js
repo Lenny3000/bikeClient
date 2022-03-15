dc29c12f4870b66cf657a9074bff0e7bc108b599
Lenny3000 <91618917+Lenny3000@users.noreply.github.com>
2022-03-15 09:43:56 -0400

    fixed APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,10 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:4000';
         break;
     case 'lrs-bike-client.herokuapp.com':
-        APIURL = 'https://lrs-bike-client.herokuapp.com'
+        APIURL = 'https://lrs-bike.herokuapp.com/'
+        break;
+        default:
+            APIURL='https://lrs-bike.herokuapp.com'
 }
 
 export default APIURL;
